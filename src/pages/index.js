1808e3c8e04f0f5a6e62a00ff0534409d258edbb
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-07 08:28:52 +0100

    index.js modified

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,14 +13,14 @@ export default function Home() {
       <main className="bg-white px-10 ">
         <section className=" min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl">MoMo</h1>
+            <h1 className="text-xl font-burtons">MoMo</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
               </li>
               <li>
                 <a
-                  className="bg-cyan-500 text-white px-4 py-2 rounded-md ml-8"
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
                   href="#"
                 >
                   Resume
@@ -28,6 +28,14 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+          <div>
+            <h2>Momoore Sontan</h2>
+            <h3>Backend Web Developer</h3>
+            <p>
+              Responsible for server-side web application logic and integration
+              of the work front-end developers do.
+            </p>
+          </div>
         </section>
       </main>
     </>
