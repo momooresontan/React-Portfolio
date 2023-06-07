441474f060290b8304038bef8e2eb6296f802dcd
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-07 08:51:55 +0100

    index.js modified

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,9 +34,11 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10 ">
-            <h2>Momoore Sontan</h2>
-            <h3>Backend Web Developer</h3>
-            <p>
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+              Momoore Sontan
+            </h2>
+            <h3 className="text-2xl py-2">Backend Web Developer</h3>
+            <p className="text-md py-5 leading-8">
               Responsible for server-side web application logic and integration
               of the work front-end developers do.
             </p>
